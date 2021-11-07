f5fbf8c70ee5c5d067cc69eee1715057feb8a17e
zchen665 <azchen665@gmail.com>
2021-11-07 09:44:05 -0600

    add rough frames

--- app/src/main/java/com/example/meowsic/MainActivity.java
+++ app/src/main/java/com/example/meowsic/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.meowsic;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,43 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button recordingButton = (Button) findViewById(R.id.home_new_timbre);
+        Button playListButton = (Button) findViewById(R.id.home_playlist);
+        Button keyBoardButoon = (Button) findViewById(R.id.home_keyboard);
+
+        recordingButton.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                toNewRecording();
+            }
+        });
+        playListButton.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                toPlayList();
+            }
+        });
+        keyBoardButoon.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                toKeyboard();
+            }
+        });
+    }
+
+
+
+    public void toNewRecording(){
+        Intent intent = new Intent(this, NewRecording.class);
+
+        startActivity(intent);
+    }
+    public void toPlayList(){
+        Intent intent = new Intent(this, PlayList.class);
+
+        startActivity(intent);
+    }
+    public void toKeyboard(){
+        Intent intent = new Intent(this, Keyboard.class);
+
+        startActivity(intent);
     }
 }
