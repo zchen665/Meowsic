7a04a7dd4300202d86e3834488bf6f619dbef263
zchen665 <azchen665@gmail.com>
2021-11-11 17:22:54 -0600

    tune change for keyboard

--- app/src/main/java/com/example/meowsic/Keyboard.java
+++ app/src/main/java/com/example/meowsic/Keyboard.java
@@ -1,13 +1,19 @@
 package com.example.meowsic;
 
+import android.annotation.SuppressLint;
 import android.media.AudioAttributes;
 import android.media.AudioManager;
 import android.media.SoundPool;
 import android.os.Bundle;
 import android.provider.MediaStore;
+import android.util.Log;
+import android.view.MotionEvent;
 import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.ArrayList;
 
 public class Keyboard extends AppCompatActivity {
 
@@ -27,11 +33,12 @@ public class Keyboard extends AppCompatActivity {
     private int assound;
     private int fssound;
 
-    private float LEFT_VOL = 1.0f;
-    private float RIGHT_VOL = 1.0f;
+    private float LEFT_VOL = 2.0f;
+    private float RIGHT_VOL = 2.0f;
     private int PRIORITY = 1;
     private int LOOP = 0;
-    private float RATE = 1.0f;
+    private float ONPRESS_RATE = 0.4f;
+    private float ONRELEASE_RATE = 1.0f;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -59,8 +66,35 @@ public class Keyboard extends AppCompatActivity {
         gssound = mSoundPool.load(getApplicationContext(),R.raw.g_hash,1);
         dssound = mSoundPool.load(getApplicationContext(),R.raw.d_hash,1);
 
+
+        Button c_btn = findViewById(R.id.note_c);
+        int[] streamIDs = new int[12];
+        c_btn.setOnTouchListener(new View.OnTouchListener(){
+            @SuppressLint("ClickableViewAccessibility")
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                if (event.getAction()== MotionEvent.ACTION_DOWN){
+                    int tmp = mSoundPool.play(csound, LEFT_VOL, RIGHT_VOL, PRIORITY, LOOP, ONPRESS_RATE);
+                    Log.i("gg", "onpress " + tmp);
+                    streamIDs[2] = tmp;
+
+                    mSoundPool.setRate(asound,ONRELEASE_RATE);
+                }
+                else if (event.getAction()== MotionEvent.ACTION_UP){
+                    Log.i("gg", "on release "+ streamIDs[2]);
+                    mSoundPool.pause(streamIDs[2]);
+                    mSoundPool.resume(streamIDs[2]);
+                }
+
+                return true;
+            }
+        });
+
     }
     public void hit_a(View view){
-        mSoundPool.play(asound,LEFT_VOL,RIGHT_VOL,PRIORITY,LOOP,RATE);
+        mSoundPool.play(asound,LEFT_VOL,RIGHT_VOL,PRIORITY,LOOP,ONPRESS_RATE);
+    }
+    public void hit_b(View view){
+//        mSoundPool.play(bsound,LEFT_VOL,RIGHT_VOL,PRIORITY,LOOP,RATE);
     }
 }
