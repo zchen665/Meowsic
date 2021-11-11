25b657fc3827bbb242fa86717db09775eb4ce4fe
lanranranM <marunduo@gmail.com>
2021-11-11 01:09:04 -0600

    update playlist page

--- app/src/main/java/com/example/meowsic/PlayList.java
+++ app/src/main/java/com/example/meowsic/PlayList.java
@@ -1,14 +1,24 @@
 package com.example.meowsic;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class PlayList extends AppCompatActivity {
+    Button addButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.playlist);
-
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.playlistaddmenu, menu);
+        return super.onCreateOptionsMenu(menu);
     }
 }
