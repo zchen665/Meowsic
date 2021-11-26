86448f7de5b789fa1cf22461231ad64889ca756b
lanranranM <marunduo@gmail.com>
2021-11-26 00:52:56 -0600

    update playlist page

--- app/src/main/java/com/example/meowsic/PlayList.java
+++ app/src/main/java/com/example/meowsic/PlayList.java
@@ -1,24 +1,33 @@
 package com.example.meowsic;
 
+import android.app.ListActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ListView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class PlayList extends AppCompatActivity {
-    Button addButton;
+import java.util.ArrayList;
+
+public class PlayList extends ListActivity {
+
+    ListView listView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.playlist);
-    }
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.playlistaddmenu, menu);
-        return super.onCreateOptionsMenu(menu);
+
+        listView = getListView();
+        ArrayList<String> nameList = new ArrayList<>();
+        // load file name as listItem
+        nameList.add("file1.mp3");
+        nameList.add("file2.mp3");
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter(this, R.layout.playlist_item,nameList);
+        listView.setAdapter(arrayAdapter);
     }
 }
