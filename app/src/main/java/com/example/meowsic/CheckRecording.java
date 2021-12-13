e2559cdb817ab3b28ef4c834bb41e58782099247
zchen665 <azchen665@gmail.com>
2021-12-12 21:41:23 -0600

    update tarosDSP lib

--- app/src/main/java/com/example/meowsic/CheckRecording.java
+++ app/src/main/java/com/example/meowsic/CheckRecording.java
@@ -1,6 +1,7 @@
 package com.example.meowsic;
 
 import android.content.Intent;
+import android.media.AudioFormat;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -9,6 +10,14 @@ import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.io.File;
+
+import be.tarsos.dsp.AudioDispatcher;
+import be.tarsos.dsp.writer.WriterProcessor;
+import be.tarsos.dsp.WaveformSimilarityBasedOverlapAdd;
+import be.tarsos.dsp.io.android.AudioDispatcherFactory;
+import be.tarsos.dsp.resample.RateTransposer;
+
 public class CheckRecording extends AppCompatActivity {
 
     @Override
@@ -16,11 +25,16 @@ public class CheckRecording extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.check_recording);
 
-        Button ret = (Button) findViewById(R.id.button5);
-
-        ret.setOnClickListener(new View.OnClickListener() {
+        Button retBtn = (Button) findViewById(R.id.button5);
+        Button replaceBtn  = (Button) findViewById(R.id.button4);
+        retBtn.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 goBack();
+            }
+        });
+        replaceBtn.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                startDSP();
             }
         });
     }
@@ -29,6 +43,46 @@ public class CheckRecording extends AppCompatActivity {
         Intent intent = new Intent(this, NewRecording.class);
         startActivity(intent);
     }
+    public void startDSP() {
+        double rate = 1.0;
+//        RateTransposer rateTransposer;
+//        AudioDispatcher dispatcher;
+//        WaveformSimilarityBasedOverlapAdd wsola;
+//
+//        dispatcher = AudioDispatcherFactory.fromPipe(mAudiopath, 44100, 5000, 2500);
+//        rateTransposer = new RateTransposer(rate);
+//        wsola = new WaveformSimilarityBasedOverlapAdd(WaveformSimilarityBasedOverlapAdd.Parameters.musicDefaults(rate, 44100));
+//        WaveformWriter writer = new WaveformWriter((TarsosDSPAudioFormat) dispatcher.getFormat(), BASE_PATH + "Recorded" + File.separator + "PITCHED_" + mVideoFileName + ".mp3");
+//
+//        wsola.setDispatcher(dispatcher);
+//        dispatcher.addAudioProcessor(wsola);
+//        dispatcher.addAudioProcessor(rateTransposer);
+//        dispatcher.addAudioProcessor(new AndroidAudioPlayer(dispatcher.getFormat()));
+//        dispatcher.setZeroPadFirstBuffer(true);
+//        dispatcher.setZeroPadLastBuffer(true);
+//        dispatcher.addAudioProcessor(writer);
+    }
+//    public void startDSP(){
+//        File inputFile = new File(source);
+//        AudioFormat format = AudioSystem.getAudioFileFormat(inputFile).getFormat();
+//        double sampleRate = format.getSampleRate();
+//        double factor = PitchShiftingExample.centToFactor(cents);
+//        RateTransposer rateTransposer = new RateTransposer(factor);
+//        WaveformSimilarityBasedOverlapAdd wsola = new WaveformSimilarityBasedOverlapAdd(WaveformSimilarityBasedOverlapAdd.Parameters.musicDefaults(factor, sampleRate));
+//        WriterProcessor writer = new WriterProcessor(outputFormat, outputFile);
+//        AudioDispatcher dispatcher;
+//        if(format.getChannels() != 1){
+//            dispatcher = AudioDispatcherFactory.fromFile(inputFile,wsola.getInputBufferSize() * format.getChannels(),wsola.getOverlap() * format.getChannels());
+//            dispatcher.addAudioProcessor(new MultichannelToMono(format.getChannels(),true));
+//        }else{
+//            dispatcher = AudioDispatcherFactory.fromFile(inputFile,wsola.getInputBufferSize(),wsola.getOverlap());
+//        }
+//        wsola.setDispatcher(dispatcher);
+//        dispatcher.addAudioProcessor(wsola);
+//        dispatcher.addAudioProcessor(rateTransposer);
+//        dispatcher.addAudioProcessor(writer);
+//        dispatcher.run();
+//    }
 
 
 }
